3a41b8938c5781d4bb1cc043e469ced975323cc3
Gabriel Victor <gv2074300@gmail.com>
2022-07-04 18:07:41 -0300

    My first commit

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Sistema de cadrasto de clientes</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <!--<link rel="stylesheet" type="text/css" href="css/editar.css">
+      <link rel="stylesheet" type="text/css" href="css/index.css">
+      <link rel="stylesheet" type="text/css" href="css/main.css">
+      <link rel="stylesheet" type="text/css" href="css/modal.css">-->
+
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    </head>
+
+    <body>
